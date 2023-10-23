4794b55aabe383304c27c5bfd8528b25b75e3b83
diegoccrs <caceresdiego2607@gmail.com>
2023-10-22 20:43:55 -0400

    commit 2 se agrego clase grafo

--- Proyecto-Suso,Caceres/src/proyecto/suso/caceres/User.java
+++ Proyecto-Suso,Caceres/src/proyecto/suso/caceres/User.java
@@ -10,12 +10,40 @@ package proyecto.suso.caceres;
  * @author Contingencia
  */
 public class User {
-    String name;
-   Lista<User> friends;
+    private String name;
+   private Lista<User> friends;
    
    public User(String name) {
         this.name = name;
         this.friends = new Lista<>();
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the friends
+     */
+    public Lista<User> getFriends() {
+        return friends;
+    }
+
+    /**
+     * @param friends the friends to set
+     */
+    public void setFriends(Lista<User> friends) {
+        this.friends = friends;
     }
     
     
